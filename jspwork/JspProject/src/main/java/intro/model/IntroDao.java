ecb37486c46cf85394b87627c949258e6e2c8742
soo <lsy971028@gmail.com>
2024-03-07 11:30:41 +0900

    angel send

--- jspwork/JspProject/src/main/java/intro/model/IntroDao.java
+++ jspwork/JspProject/src/main/java/intro/model/IntroDao.java
@@ -84,4 +84,67 @@ public class IntroDao {
 		return list;
 	}
 	
+	//detailView..num에 해당하는 하나의 dto반환
+	public IntroDto getOneData(String num)
+	{
+		IntroDto dto=new IntroDto();
+		
+		Connection conn=db.getConnection();
+		PreparedStatement pstmt=null;
+		ResultSet rs=null;
+		
+		String sql="select * from intro where num=?";
+		
+		try {
+			pstmt=conn.prepareStatement(sql);
+			pstmt.setString(1, num);
+			rs=pstmt.executeQuery();
+			
+			if(rs.next())
+			{
+				dto.setNum(rs.getString("num"));
+				dto.setName(rs.getString("name"));
+				dto.setBirthday(rs.getString("birthday"));
+				dto.setHobby(rs.getString("hobby"));
+				dto.setHometown(rs.getString("hometown"));
+				dto.setMemo(rs.getString("memo"));
+				dto.setAge(rs.getString("age"));
+				
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			db.dbClose(rs, pstmt, conn);
+		}
+		
+		
+		return dto;
+	}
+	
+	//수정(나이,생년월일,거주지역,취미,성격)
+	
+	
+	
+	//삭제 (시퀀스에 해당 하는 db삭제)
+	public void deleteIntro(String num)
+	{
+		Connection conn=db.getConnection();
+		PreparedStatement pstmt=null;
+		
+		String sql="delete from intro where num=?";
+		
+		try {
+			pstmt=conn.prepareStatement(sql);
+			pstmt.setString(1, num);
+			pstmt.execute();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			db.dbClose(pstmt, conn);
+		}
+		
+	}
+	
 }
