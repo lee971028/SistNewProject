80c4708329a3951a8b0ab1e48bcca4243eb55e44
soo <lsy971028@gmail.com>
2024-03-08 11:27:04 +0900

    day0308

--- jspwork/JspProject/src/main/java/myworld/model/WorldDao.java
+++ jspwork/JspProject/src/main/java/myworld/model/WorldDao.java
@@ -98,4 +98,66 @@ public class WorldDao {
 		
 	}
 	
+	//num에 해당하는 dto반환
+	public WorldDto getData(String num)
+	{
+		WorldDto dto=new WorldDto();
+		
+		Connection conn=db.getConnection();
+		PreparedStatement pstmt=null;
+		ResultSet rs=null;
+		
+		String sql="select * from myworld where num=?";
+		
+		try {
+			pstmt=conn.prepareStatement(sql);
+			pstmt.setString(1, num);
+			rs=pstmt.executeQuery();
+			
+			if(rs.next())
+			{
+				dto.setNum(rs.getString("num"));
+				dto.setWriter(rs.getString("writer"));
+				dto.setContent(rs.getString("content"));
+				dto.setAvata(rs.getString("avata"));
+				dto.setWriteday(rs.getTimestamp("writeday"));
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			db.dbClose(rs, pstmt, conn);
+		}
+		
+		
+		return dto;
+	}
+	
+	
+	//수정
+	public void updateWorld(WorldDto dto)
+	{
+		Connection conn=db.getConnection();
+		PreparedStatement pstmt=null;
+		String sql="update myworld set writer=?,content=?,avata=? where num=?";
+		
+		try {
+			pstmt=conn.prepareStatement(sql);
+			
+			//바인딩
+			pstmt.setString(1, dto.getWriter());
+			pstmt.setString(2, dto.getContent());
+			pstmt.setString(3, dto.getAvata());
+			pstmt.setString(4, dto.getNum());
+			
+			pstmt.execute();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			db.dbClose(pstmt, conn);
+		}
+		
+	}
+	
 }
