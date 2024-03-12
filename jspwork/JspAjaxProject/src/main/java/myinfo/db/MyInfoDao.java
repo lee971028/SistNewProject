86ab0fa450c47afa8ad9d2535954a27b76596f9e
soo <lsy971028@gmail.com>
2024-03-12 12:50:45 +0900

    day0308

---
+++ jspwork/JspAjaxProject/src/main/java/myinfo/db/MyInfoDao.java
@@ -0,0 +1,161 @@
+package myinfo.db;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import mysql.db.DbConnect;
+
+public class MyInfoDao {
+
+	DbConnect db=new DbConnect();
+	
+	public void insertMyInfo(MyInfoDto dto)
+	{
+		Connection conn=db.getConnection();
+		PreparedStatement pstmt=null;
+		
+		String sql="insert into myinfo values(null,?,?,?,?,now())";
+		
+		try {
+			pstmt=conn.prepareStatement(sql);
+			
+			pstmt.setString(1, dto.getName());
+			pstmt.setString(2, dto.getHp());
+			pstmt.setString(3, dto.getBlood());
+			pstmt.setDate(4, dto.getBirth());
+			
+			pstmt.execute();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			db.dbClose(pstmt, conn);
+		}
+		
+	}
+	
+	public List<MyInfoDto> getAllInfos()
+	{
+		List<MyInfoDto> list=new ArrayList<MyInfoDto>();
+		
+		Connection conn=db.getConnection();
+		PreparedStatement pstmt=null;
+		ResultSet rs=null;
+		
+		String sql="select * from myinfo order by num desc";
+		
+		try {
+			pstmt=conn.prepareStatement(sql);
+			rs=pstmt.executeQuery();
+			
+			while(rs.next())
+			{
+				MyInfoDto dto=new MyInfoDto();
+				
+				dto.setNum(rs.getString("num"));
+				dto.setName(rs.getString("name"));
+				dto.setBlood(rs.getString("blood"));
+				dto.setHp(rs.getString("hp"));
+				dto.setBirth(rs.getDate("birth"));
+				dto.setWriteday(rs.getTimestamp("writeday"));
+				
+				list.add(dto);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			db.dbClose(rs, pstmt, conn);
+		}
+		
+		
+		return list;
+	}
+	
+	public void deleteInfo(String num)
+	{
+		Connection conn=db.getConnection();
+		PreparedStatement pstmt=null;
+		
+		String sql="delete from myinfo where num=?";
+		
+		try {
+			pstmt=conn.prepareStatement(sql);
+			
+			pstmt.setString(1, num);
+			pstmt.execute();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			db.dbClose(pstmt, conn);
+		}
+		
+	}
+	
+	//num에 대한 dto반환
+	public MyInfoDto getOneData(String num)
+	{
+		MyInfoDto dto=new MyInfoDto();
+		
+		Connection conn=db.getConnection();
+		PreparedStatement pstmt=null;
+		ResultSet rs=null;
+		
+		String sql="select * from myinfo where num=?";
+		
+		try {
+			pstmt=conn.prepareStatement(sql);
+			pstmt.setString(1, num);
+			rs=pstmt.executeQuery();
+			
+			if(rs.next())
+			{
+				dto.setNum(rs.getString("num"));
+				dto.setName(rs.getString("name"));
+				dto.setBlood(rs.getString("blood"));
+				dto.setHp(rs.getString("hp"));
+				dto.setBirth(rs.getDate("birth"));
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			db.dbClose(rs, pstmt, conn);
+		}
+		
+		
+		return dto;
+	}
+	
+	
+	//수정
+	public void updateInfo(MyInfoDto dto)
+	{
+		Connection conn=db.getConnection();
+		PreparedStatement pstmt=null;
+		
+		String sql="update myinfo set name=?,blood=?,hp=?,birth=? where num=?";
+		
+		try {
+			pstmt=conn.prepareStatement(sql);
+			
+			pstmt.setString(1, dto.getName());
+			pstmt.setString(2, dto.getBlood());
+			pstmt.setString(3, dto.getHp());
+			pstmt.setDate(4, dto.getBirth());
+			pstmt.setString(5, dto.getNum());
+			pstmt.execute();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			db.dbClose(pstmt, conn);
+		}
+		
+	}
+}
